5233399432adc15f5ee5f3e0accf5d85acb36b2d
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2025-06-23 07:35:48 -0500

    fix: imports con .js para módulos ES

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createProduct } from "../controllers/productController.js";
+import { createProduct, getProducts } from "../controllers/productController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
-
+router.get("/", getProducts);
 router.post("/", protect, createProduct); // <-- esta línea maneja POST /api/products
 
 export default router;
