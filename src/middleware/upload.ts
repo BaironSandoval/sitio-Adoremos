b688d80b713db2aea03193bbed4ee7ed314e9629
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2025-06-24 08:15:21 -0500

    fix: imports con .js para módulos ES

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,5 +1,7 @@
 import multer from "multer";
-import { storage } from "../utils/cloudinary.js";
+//import { storage } from "../utils/cloudinary.js";
+
+const storage = multer.memoryStorage(); // Usamos memoria para manejar el buffer de la imagen
 
 const upload = multer({ storage }); // CloudinaryStorage ya sube la imagen
 
