67b211812b34ad1f3cfa18d77fbdff92179a453a
BaironSandoval <baironalfonsosandovalnino@gmail.com>
2025-06-25 06:25:53 -0500

    agregue nuevas rutas para editar y eliminar blogs

--- src/models/BlogPost.ts
+++ src/models/BlogPost.ts
@@ -4,6 +4,7 @@ export interface IBlogPost extends Document {
   title: string;
   content: string;
   author: string;
+  image: string;
   createdAt: Date;
 }
 
@@ -11,6 +12,7 @@ const BlogPostSchema: Schema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: String, required: true },
+  image: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
